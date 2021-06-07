c5ed5e7da01ae9580753224b64eb2a63feed573d
mchan <mchan@JEQPVA.jeqpva.local.com>
2021-06-06 23:00:05 -0400

    Refactor code: use pcap_findalldevs(...)

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+CC = gcc
+CCFLAG = -g -Wall
+
 all: hello_pcap.c
 	# gcc -g -Wall -o hello_pcap hello_pcap.c -L/usr/local/lib -lpcap
-	gcc -g -Wall -o hello_pcap hello_pcap.c -lpcap 
+	${CC} ${CCFLAG} -o hello_pcap hello_pcap.c -lpcap 
 
-
+.PHONY: clean
 clean:
 	rm -rf *.o hello_pcap
--- hello_pcap.c
+++ hello_pcap.c
@@ -1,30 +1,24 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    char errBuf[PCAP_ERRBUF_SIZE], *dev;
+    char errbuf[PCAP_ERRBUF_SIZE];
     pcap_if_t *it;
-    int ret;
 
-    // dev = pcap_lookupdev(errBuf);
-    ret = pcap_findalldevs(&it, errBuf);
-    if(ret == -1) {
-        printf("error:%s\n", errBuf);
+    if(pcap_findalldevs(&it, errbuf) != 0) {
+        while (it) {
+            printf("%s - %s\n", it->name, it->description);
+            // printf("%s\n", iptos(it->addresses->addr));
+            it = it->next;
+        }
+        pcap_freealldevs(it);
+    }
+    else {
+        printf("error: find all devs - %s\n", errbuf);
         exit(-1);
     }
-    while (it) {
-        printf(":%s - %s\n", it->name, it->description);
-        printf("%s\n", iptos(it->addresses->addr));
-        it = it->next;
-    }
-    
-    // if(dev){
-    //     printf("success: device: %s\n", dev);
-    // }
-    // else {
-    //     printf("error: %s\n", errBuf);
-    // }
 
     return 0;
 }
