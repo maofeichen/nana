79d088a5ccd4a3e74dd9b1e6ceb85b8ae9c8e423
mchen <mfchen@outlook.com>
2021-07-28 06:10:13 -0400

    Rename na.c -> nm.c

--- src/nm.c
+++ src/nm.c
@@ -6,6 +6,7 @@ void usage(char *n)
 {
     printf("usage: %s [options]\n"
            "Options:\n"
+           "--iface|-i          Capture live network traffice from iface\n"
            "--print|-p          Display network interface information\n"
            "--help|-h           Display help information\n"
            "--version|-v        Display version informaion\n", n);
@@ -20,6 +21,7 @@ int main(int argc, char *argv[])
 {
     int opt, opt_index;
     static const struct option longopts[] = {
+        {"iface",   required_argument, 0, 'i'},
         {"print",   no_argument, 0, 'p'},
         {"help",    no_argument, 0, 'h'},
         {"version", no_argument, 0, 'v'},
@@ -30,10 +32,13 @@ int main(int argc, char *argv[])
         usage(argv[0]);
     }
 
-    while ((opt = getopt_long_only(argc, argv, "phv", longopts, &opt_index)) != -1) {
-        printf("opt_index: %d - option: %s\n", opt_index, longopts[opt_index].name);
+    while ((opt = getopt_long_only(argc, argv, "phvi:", longopts, &opt_index)) != -1) {
+        // printf("opt_index: %d - option: %s\n", opt_index, longopts[opt_index].name);
         switch (opt)
         {
+        case 'i':
+            capture_live(optarg);
+            break;
         case 'p':
             print_alldevs();
             break;
@@ -50,6 +55,6 @@ int main(int argc, char *argv[])
     for(int i = optind; i < argc; i++) {
         fprintf(stderr, "%s: invalid positional argument: %s\n", argv[0], argv[i]);
     }
-
+    printf("finished\n");
     exit(0);
 }
